4c72566f0ba4723c21d958158b8782ea56a3bb35
CodeSandbox <support@codesandbox.io>
2025-10-03 20:45:05 +0000

    Sandbox snapshot commit

---
+++ kirestein/embalagens-com-classe/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          primary: '#1d4ed8',
+          accent: '#f59e0b',
+          secondary: '#16a34a'
+        }
+      }
+    }
+  },
+  plugins: []
+};
